7f84d7a9b8df449e0922e9cedc46e6d5da6abe1f
Doniyorbek <doniyorbekxakimbekov7007@gmail.com>
2022-09-15 00:07:48 +0300

    INFRA: Initialize Unit Test Project

---
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,14 @@
+﻿//====================================
+//Copyright (c) Coalition of Doog-Hearted Engineers
+//Free to Use Comfort and Peace
+//====================================
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        void ShouldBeTrue() => Assert.True(true);
+    }
+}
